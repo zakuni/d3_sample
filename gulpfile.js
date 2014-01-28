11f684c5d6f0f5b7fb2a72b07784a9a9b587112c
zakuni <kunio038@gmail.com>
2014-01-28 21:41:45 +0900

    use gulp to compile jade

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var coffee = require('gulp-coffee');
+var jade = require('gulp-jade');
+
+gulp.task('jade', function() {
+  gulp.src('*.jade')
+    .pipe(jade({pretty: true}))
+    .pipe(gulp.dest('./'))
+}); 
 
 gulp.task('coffee', function() {
   gulp.src('*.coffee')
@@ -9,5 +16,6 @@ gulp.task('coffee', function() {
 });
 
 gulp.task('default', function(){
+  gulp.watch(['*.jade'], ['jade']);
   gulp.watch(['*.coffee'], ['coffee']);
 });
