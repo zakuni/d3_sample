8ec7f79e9c831be9e50479afaf345f3c9c734b03
zakuni <kunio038@gmail.com>
2014-01-31 01:13:54 +0900

    rewrite bar graph with svg

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
 (function() {
-  var data, x;
+  var bar, barHeight, chart, data, width, x;
 
   data = [4, 8, 15, 16, 23, 42];
 
-  x = d3.scale.linear().domain([0, d3.max(data)]).range([0, 420]);
+  width = 420;
 
-  d3.select(".chart").selectAll("div").data(data).enter().append("div").style("width", function(d) {
-    return x(d) + "px";
-  }).text(function(d) {
+  barHeight = 20;
+
+  x = d3.scale.linear().domain([0, d3.max(data)]).range([0, width]);
+
+  chart = d3.select(".chart").attr("width", width).attr("height", barHeight * data.length);
+
+  bar = chart.selectAll("g").data(data).enter().append("g").attr("transform", function(d, i) {
+    return "translate(0, " + (i * barHeight) + ")";
+  });
+
+  bar.append("rect").attr("width", x).attr("height", barHeight - 1);
+
+  bar.append("text").attr("x", function(d) {
+    return x(d) - 3;
+  }).attr("y", barHeight / 2).attr("dy", ".35em").text(function(d) {
     return d;
   });
 
