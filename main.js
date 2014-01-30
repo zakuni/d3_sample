8a29c6492d46ebbe9048323e22fe99fa652a1168
zakuni <kunio038@gmail.com>
2014-01-31 00:54:23 +0900

    bar graph with html

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 (function() {
-  var data;
+  var data, x;
 
   data = [4, 8, 15, 16, 23, 42];
 
+  x = d3.scale.linear().domain([0, d3.max(data)]).range([0, 420]);
+
   d3.select(".chart").selectAll("div").data(data).enter().append("div").style("width", function(d) {
-    return d * 10 + "px";
+    return x(d) + "px";
   }).text(function(d) {
     return d;
   });
