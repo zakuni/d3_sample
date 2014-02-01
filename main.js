42c9e880e081b230c49ab09a6eda85534c593fbd
zakuni <kunio038@gmail.com>
2014-02-01 15:21:31 +0900

    read data from tsv

--- main.js
+++ main.js
@@ -1,27 +1,39 @@
 (function() {
-  var bar, barHeight, chart, data, width, x;
-
-  data = [4, 8, 15, 16, 23, 42];
+  var barHeight, chart, type, width, x;
 
   width = 420;
 
   barHeight = 20;
 
-  x = d3.scale.linear().domain([0, d3.max(data)]).range([0, width]);
+  x = d3.scale.linear().range([0, width]);
 
-  chart = d3.select(".chart").attr("width", width).attr("height", barHeight * data.length);
+  chart = d3.select(".chart").attr("width", width);
 
-  bar = chart.selectAll("g").data(data).enter().append("g").attr("transform", function(d, i) {
-    return "translate(0, " + (i * barHeight) + ")";
+  d3.tsv("data.tsv", type, function(error, data) {
+    var bar;
+    x.domain([
+      0, d3.max(data, function(d) {
+        return d.value;
+      })
+    ]);
+    chart.attr("height", barHeight * data.length);
+    bar = chart.selectAll("g").data(data).enter().append("g").attr("transform", function(d, i) {
+      return "translate(0, " + (i * barHeight) + ")";
+    });
+    bar.append("rect").attr("width", function(d) {
+      return x(d.value);
+    }).attr("height", barHeight - 1);
+    return bar.append("text").attr("x", function(d) {
+      return x(d.value) - 3;
+    }).attr("y", barHeight / 2).attr("dy", ".35em").text(function(d) {
+      return d.value;
+    });
   });
 
-  bar.append("rect").attr("width", x).attr("height", barHeight - 1);
-
-  bar.append("text").attr("x", function(d) {
-    return x(d) - 3;
-  }).attr("y", barHeight / 2).attr("dy", ".35em").text(function(d) {
+  type = function(d) {
+    d.value = +d.value;
     return d;
-  });
+  };
 
 }).call(this);
 
