e6b1e9751c8bb485708af8af363a85f9fbc583d0
zakuni <kunio038@gmail.com>
2014-02-01 22:08:58 +0900

    rotate chart

--- main.coffee
+++ main.coffee
@@ -1,37 +1,43 @@
-width = 420
-barHeight = 20
+width = 900
+height = 500
 
 x = 
   d3.scale.linear()
     .range([0, width])
 
+y =
+  d3.scale.linear()
+    .range([height, 0])
+
 chart = 
   d3.select(".chart")
     .attr("width", width)
+    .attr("height", height)
 
 d3.tsv "data.tsv",
   (d) ->
     type(d) 
   ,
   (error, data) ->
-    x.domain([0, d3.max(data, (d) -> d.value)])
+    y.domain([0, d3.max(data, (d) -> d.value)])
 
-    chart.attr("height", barHeight * data.length)
+    barWidth = width / data.length
 
     bar = 
       chart.selectAll("g")
         .data(data)
       .enter().append("g")
-        .attr("transform", (d, i) -> "translate(0, #{i * barHeight})")
+        .attr("transform", (d, i) -> "translate(#{i * barWidth}, 0)")
 
     bar.append("rect")
-      .attr("width", (d) -> x(d.value))
-      .attr("height", barHeight - 1)
+      .attr("y", (d) -> y(d.value))
+      .attr("height", (d) -> height - y(d.value))
+      .attr("width", barWidth - 1)
 
     bar.append("text")
-      .attr("x", (d) -> x(d.value) - 3)
-      .attr("y", barHeight / 2)
-      .attr("dy", ".35em")
+      .attr("x", barWidth / 2)
+      .attr("y", (d) -> y(d.value) + 3)
+      .attr("dy", ".75em")
       .text((d) -> d.value)
 
 type = (d) ->
--- main.js
+++ main.js
@@ -1,33 +1,37 @@
 (function() {
-  var barHeight, chart, type, width, x;
+  var chart, height, type, width, x, y;
 
-  width = 420;
+  width = 900;
 
-  barHeight = 20;
+  height = 500;
 
   x = d3.scale.linear().range([0, width]);
 
-  chart = d3.select(".chart").attr("width", width);
+  y = d3.scale.linear().range([height, 0]);
+
+  chart = d3.select(".chart").attr("width", width).attr("height", height);
 
   d3.tsv("data.tsv", function(d) {
     return type(d);
   }, function(error, data) {
-    var bar;
-    x.domain([
+    var bar, barWidth;
+    y.domain([
       0, d3.max(data, function(d) {
         return d.value;
       })
     ]);
-    chart.attr("height", barHeight * data.length);
+    barWidth = width / data.length;
     bar = chart.selectAll("g").data(data).enter().append("g").attr("transform", function(d, i) {
-      return "translate(0, " + (i * barHeight) + ")";
+      return "translate(" + (i * barWidth) + ", 0)";
     });
-    bar.append("rect").attr("width", function(d) {
-      return x(d.value);
-    }).attr("height", barHeight - 1);
-    return bar.append("text").attr("x", function(d) {
-      return x(d.value) - 3;
-    }).attr("y", barHeight / 2).attr("dy", ".35em").text(function(d) {
+    bar.append("rect").attr("y", function(d) {
+      return y(d.value);
+    }).attr("height", function(d) {
+      return height - y(d.value);
+    }).attr("width", barWidth - 1);
+    return bar.append("text").attr("x", barWidth / 2).attr("y", function(d) {
+      return y(d.value) + 3;
+    }).attr("dy", ".75em").text(function(d) {
       return d.value;
     });
   });
