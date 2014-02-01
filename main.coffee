c62c1aa89c77bf6b90427db4156a69df3a30cf52
zakuni <kunio038@gmail.com>
2014-02-01 22:18:55 +0900

    use d3 ordinal

--- main.coffee
+++ main.coffee
@@ -2,8 +2,8 @@ width = 900
 height = 500
 
 x = 
-  d3.scale.linear()
-    .range([0, width])
+  d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1)
 
 y =
   d3.scale.linear()
@@ -19,23 +19,22 @@ d3.tsv "data.tsv",
     type(d) 
   ,
   (error, data) ->
+    x.domain(data.map((d) -> d.name))
     y.domain([0, d3.max(data, (d) -> d.value)])
-
-    barWidth = width / data.length
 
     bar = 
       chart.selectAll("g")
         .data(data)
       .enter().append("g")
-        .attr("transform", (d, i) -> "translate(#{i * barWidth}, 0)")
+        .attr("transform", (d, i) -> "translate(#{x(d.name)}, 0)")
 
     bar.append("rect")
       .attr("y", (d) -> y(d.value))
       .attr("height", (d) -> height - y(d.value))
-      .attr("width", barWidth - 1)
+      .attr("width", x.rangeBand())
 
     bar.append("text")
-      .attr("x", barWidth / 2)
+      .attr("x", x.rangeBand() / 2)
       .attr("y", (d) -> y(d.value) + 3)
       .attr("dy", ".75em")
       .text((d) -> d.value)
--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 
   height = 500;
 
-  x = d3.scale.linear().range([0, width]);
+  x = d3.scale.ordinal().rangeRoundBands([0, width], .1);
 
   y = d3.scale.linear().range([height, 0]);
 
@@ -14,22 +14,24 @@
   d3.tsv("data.tsv", function(d) {
     return type(d);
   }, function(error, data) {
-    var bar, barWidth;
+    var bar;
+    x.domain(data.map(function(d) {
+      return d.name;
+    }));
     y.domain([
       0, d3.max(data, function(d) {
         return d.value;
       })
     ]);
-    barWidth = width / data.length;
     bar = chart.selectAll("g").data(data).enter().append("g").attr("transform", function(d, i) {
-      return "translate(" + (i * barWidth) + ", 0)";
+      return "translate(" + (x(d.name)) + ", 0)";
     });
     bar.append("rect").attr("y", function(d) {
       return y(d.value);
     }).attr("height", function(d) {
       return height - y(d.value);
-    }).attr("width", barWidth - 1);
-    return bar.append("text").attr("x", barWidth / 2).attr("y", function(d) {
+    }).attr("width", x.rangeBand());
+    return bar.append("text").attr("x", x.rangeBand() / 2).attr("y", function(d) {
       return y(d.value) + 3;
     }).attr("dy", ".75em").text(function(d) {
       return d.value;
